a365493341f877de7f6760b4f48ae567d6628789
George Stratig <geo.stratigopoulos@gmail.com>
2021-06-03 18:40:06 +0300

    Adding Mobile app.

---
+++ MobileApp/Login/LoginForm.Designer.cs
@@ -0,0 +1,146 @@
+﻿
+namespace Login
+{
+    partial class LoginForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(LoginForm));
+            this.usernameLabel = new System.Windows.Forms.Label();
+            this.passwordLabel = new System.Windows.Forms.Label();
+            this.usernameTextbox = new System.Windows.Forms.TextBox();
+            this.passwordTextbox = new System.Windows.Forms.TextBox();
+            this.connectButton = new System.Windows.Forms.Button();
+            this.registerButton = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // usernameLabel
+            // 
+            this.usernameLabel.AutoSize = true;
+            this.usernameLabel.Location = new System.Drawing.Point(85, 410);
+            this.usernameLabel.Name = "usernameLabel";
+            this.usernameLabel.Size = new System.Drawing.Size(132, 25);
+            this.usernameLabel.TabIndex = 1;
+            this.usernameLabel.Text = "Όνομα χρήστη";
+            // 
+            // passwordLabel
+            // 
+            this.passwordLabel.AutoSize = true;
+            this.passwordLabel.Location = new System.Drawing.Point(85, 496);
+            this.passwordLabel.Name = "passwordLabel";
+            this.passwordLabel.Size = new System.Drawing.Size(178, 25);
+            this.passwordLabel.TabIndex = 2;
+            this.passwordLabel.Text = "Κωδικός πρόσβασης";
+            // 
+            // usernameTextbox
+            // 
+            this.usernameTextbox.Location = new System.Drawing.Point(89, 440);
+            this.usernameTextbox.MaxLength = 30;
+            this.usernameTextbox.Name = "usernameTextbox";
+            this.usernameTextbox.Size = new System.Drawing.Size(334, 31);
+            this.usernameTextbox.TabIndex = 3;
+            // 
+            // passwordTextbox
+            // 
+            this.passwordTextbox.Location = new System.Drawing.Point(89, 525);
+            this.passwordTextbox.MaxLength = 30;
+            this.passwordTextbox.Name = "passwordTextbox";
+            this.passwordTextbox.Size = new System.Drawing.Size(334, 31);
+            this.passwordTextbox.TabIndex = 4;
+            this.passwordTextbox.UseSystemPasswordChar = true;
+            // 
+            // connectButton
+            // 
+            this.connectButton.BackColor = System.Drawing.Color.Red;
+            this.connectButton.Location = new System.Drawing.Point(89, 576);
+            this.connectButton.Name = "connectButton";
+            this.connectButton.Size = new System.Drawing.Size(128, 42);
+            this.connectButton.TabIndex = 5;
+            this.connectButton.Text = "Σύνδεση";
+            this.connectButton.UseVisualStyleBackColor = false;
+            this.connectButton.Click += new System.EventHandler(this.ConnectButton_Click);
+            // 
+            // registerButton
+            // 
+            this.registerButton.BackColor = System.Drawing.Color.Red;
+            this.registerButton.Location = new System.Drawing.Point(294, 576);
+            this.registerButton.Name = "registerButton";
+            this.registerButton.Size = new System.Drawing.Size(127, 42);
+            this.registerButton.TabIndex = 6;
+            this.registerButton.Text = "Εγγραφή";
+            this.registerButton.UseVisualStyleBackColor = false;
+            this.registerButton.Click += new System.EventHandler(this.RegisterButton_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(89, 48);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(334, 291);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 7;
+            this.pictureBox1.TabStop = false;
+            // 
+            // LoginForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.ClientSize = new System.Drawing.Size(508, 711);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.registerButton);
+            this.Controls.Add(this.connectButton);
+            this.Controls.Add(this.passwordTextbox);
+            this.Controls.Add(this.usernameTextbox);
+            this.Controls.Add(this.passwordLabel);
+            this.Controls.Add(this.usernameLabel);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "LoginForm";
+            this.ShowIcon = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Load += new System.EventHandler(this.LoginForm_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label usernameLabel;
+        private System.Windows.Forms.Label passwordLabel;
+        private System.Windows.Forms.TextBox usernameTextbox;
+        private System.Windows.Forms.TextBox passwordTextbox;
+        private System.Windows.Forms.Button connectButton;
+        private System.Windows.Forms.Button registerButton;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
---
+++ MobileApp/Login/LoginForm.cs
@@ -0,0 +1,178 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Diagnostics;
+using Project.ClientForms;
+using Project.Login;
+using Project.BusDriverForms;
+using ClassesFolder;
+
+namespace Login
+{
+    public partial class LoginForm : Form
+    {
+        public LoginForm()
+        {
+            InitializeComponent();
+        }
+
+        private void LoginForm_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void RegisterButton_Click(object sender, EventArgs e)
+        {
+            RegisterForm form = new RegisterForm();
+            form.ShowDialog();
+        }
+
+        private void ConnectButton_Click(object sender, EventArgs e)
+        {
+            if (usernameTextbox.Text != "" && passwordTextbox.Text != "")
+            {
+                var existsInDatabase = FindExistanceInDatabase(usernameTextbox.Text, passwordTextbox.Text);
+
+                if (existsInDatabase.Item1)
+                {
+                    if (existsInDatabase.Item2 == "client")
+                    {
+                        ClientForm form = new ClientForm(FindClientInfo(usernameTextbox.Text));
+                        form.ShowDialog();
+                    }
+                    else if (existsInDatabase.Item2 == "bus_driver")
+                    {
+                        BusDriverForm form = new BusDriverForm(FindBusDriverInfo(usernameTextbox.Text));
+                        form.ShowDialog();
+                    }
+                    else
+                    {
+                        MessageBox.Show("Λάθος όνομα χρήση ή κωδικός πρόσβασης.", 
+                                        "Σφάλμα", 
+                                        MessageBoxButtons.OK, 
+                                        MessageBoxIcon.Error);
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("Λάθος όνομα χρήση ή κωδικός πρόσβασης.", 
+                                    "Σφάλμα", 
+                                    MessageBoxButtons.OK,
+                                    MessageBoxIcon.Error);
+                }
+            }
+            else
+            {
+                MessageBox.Show("Παρακαλώ συμπληρώστε όλα τα πεδία.",
+                                "Σφάλμα",
+                                MessageBoxButtons.OK,
+                                MessageBoxIcon.Error);
+            }
+        }
+
+        private (bool, string) FindExistanceInDatabase(string username, string password)
+        {
+            try
+            {
+                using var connection = new MySqlConnection(ConnectionInfo.ConnectionString);
+                connection.Open();
+                var statement = @"select count(*), property 
+                                  from user 
+                                  where username = @username and password = EncryptPassword(@password);";
+                using var cmd = new MySqlCommand(statement, connection);
+
+                cmd.Parameters.AddWithValue("@username", username);
+                cmd.Parameters.AddWithValue("@password", password);
+                using MySqlDataReader reader = cmd.ExecuteReader();
+                reader.Read();
+
+                return (reader.GetInt32(0) == 1, reader.IsDBNull(1) ? "" : reader.GetString(1));
+            }
+            catch (MySqlException)
+            {
+                MessageBox.Show("Προκλήθηκε σφάλμα κατά την σύνδεση με τον server. Η εφαρμογή θα τερματιστεί!", 
+                                "Σφάλμα", 
+                                MessageBoxButtons.OK, 
+                                MessageBoxIcon.Error);
+                this.Close();
+                return (false, "");
+            }
+        }
+
+        private Client FindClientInfo(string username)
+        {
+            try
+            {
+                using var connection = new MySqlConnection(ConnectionInfo.ConnectionString);
+                connection.Open();
+                var statement = @"select name, surname
+                                  from user
+                                  where username = @username;";
+                using var cmd = new MySqlCommand(statement, connection);
+
+                cmd.Parameters.AddWithValue("@username", username);
+                using MySqlDataReader reader = cmd.ExecuteReader();
+                reader.Read();
+
+                return new Client(username,
+                                  reader.GetString(0),
+                                  reader.GetString(1),
+                                  "Client");
+            }
+            catch (MySqlException)
+            {
+                MessageBox.Show("Προκλήθηκε σφάλμα κατά την σύνδεση με τον server. Η εφαρμογή θα τερματιστεί!",
+                                "Σφάλμα",
+                                MessageBoxButtons.OK,
+                                MessageBoxIcon.Error);
+                this.Close();
+                return null;
+            }
+        }
+   
+        private BusDriver FindBusDriverInfo(string username)
+        {
+            try
+            {
+                using var connection = new MySqlConnection(ConnectionInfo.ConnectionString);
+                connection.Open();
+                var statement = @"select name, surname, salary, experience, hireDate, complaintsCounter, availableWorkingHours
+                                  from user 
+                                  inner join Employee on User.username = Employee.username
+                                  inner join BusDriver on User.username = BusDriver.username
+                                  where User.username = @username;";
+                using var cmd = new MySqlCommand(statement, connection);
+
+                cmd.Parameters.AddWithValue("@username", username);
+                using MySqlDataReader reader = cmd.ExecuteReader();
+                reader.Read();
+
+                return new BusDriver(username,
+                                     reader.GetString(0),
+                                     reader.GetString(1),
+                                     "Bus Driver",
+                                     reader.GetDecimal(2),
+                                     reader.GetInt32(3),
+                                     reader.GetString(4),
+                                     reader.GetInt32(5),
+                                     reader.GetInt32(6));
+            }
+            catch (MySqlException)
+            {
+                MessageBox.Show("Προκλήθηκε σφάλμα κατά την σύνδεση με τον server. Η εφαρμογή θα τερματιστεί!",
+                                "Σφάλμα",
+                                MessageBoxButtons.OK,
+                                MessageBoxIcon.Error);
+                this.Close();
+                return null;
+            }
+        }
+    }
+}
